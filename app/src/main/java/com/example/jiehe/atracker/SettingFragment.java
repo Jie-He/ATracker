b1fca004852233abf749689f6ca351157b8f3540
Jie He <Jiehe1998yd@gmail.com>
2018-03-14 15:31:30 +0000

    Small UI changes

--- app/src/main/java/com/example/jiehe/atracker/SettingFragment.java
+++ app/src/main/java/com/example/jiehe/atracker/SettingFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by jiehe on 12/03/2018.
@@ -13,10 +14,14 @@ import android.view.ViewGroup;
 
 public class SettingFragment extends Fragment {
 
+    Button btnSave;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         //return super.onCreateView(inflater, container,savedInstanceState);
         View view = inflater.inflate(R.layout.frag_setting, container, false);
+        btnSave = (Button)view.findViewById(R.id.btnSave);
+
         return view;
     }
 
