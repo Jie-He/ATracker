f354b1c29639934bea8d1fca4b008ee1b33077ec
Jie He <Jiehe1998yd@gmail.com>
2018-04-20 00:01:14 +0100

    Goal page partially done

--- app/src/main/java/com/example/jiehe/atracker/GoalFragment.java
+++ app/src/main/java/com/example/jiehe/atracker/GoalFragment.java
@@ -7,23 +7,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jiehe on 14/03/2018.
  */
 
 public class GoalFragment extends Fragment{
-    private EditText namebox;
-    private EditText goalbox;
-    private CheckBox limitcheck;
-    private ListView listview2;
+
+    private Button btnRemove;
+    private Button btnCreate;
+    private ListView activityList;
     private FileManager fm;
-    private FloatingActionButton fab;
-    private FloatingActionButton fab2;
+
+    NewActivity na;
 
     public void setFM(FileManager f){
         fm = f;
@@ -34,12 +39,34 @@ public class GoalFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         //return super.onCreateView(inflater, container,savedInstanceState);
         View view = inflater.inflate(R.layout.frag_goal, container, false);
-        namebox = (EditText)view.findViewById(R.id.namebox);
-        goalbox = (EditText)view.findViewById(R.id.goalbox);
-        limitcheck = (CheckBox)view.findViewById(R.id.limitcheck);
-        listview2 = (ListView)view.findViewById(R.id.listview1);
-        fab = (FloatingActionButton)view.findViewById(R.id.addactivity);
-        fab2 = (FloatingActionButton)view.findViewById(R.id.addactivity2);
+
+        btnCreate = (Button)view.findViewById(R.id.btnAddGoal);
+        btnRemove = (Button)view.findViewById(R.id.btnRemoveGoal);
+        activityList = (ListView)view.findViewById(R.id.listGoal);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                na = new NewActivity();
+                na.show(getFragmentManager(), "New Activity");
+            }
+        });
+
+        loadActivities();
         return view;
+    }
+
+    public void loadActivities(){
+        ArrayList<MyActivity> ma = fm.getActivity();
+        if(ma.size() > 0 ){
+            List<String> names = new ArrayList<>();
+            for(MyActivity m : ma){
+                names.add(m.getName());
+            }
+
+            ArrayAdapter<String> aAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, names);
+
+            activityList.setAdapter(aAdapter);
+        }
     }
 }
