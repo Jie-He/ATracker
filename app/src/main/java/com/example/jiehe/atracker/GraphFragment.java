ba595ef1ad22a4efcbec4978eba8bc8508a92283
Jie He <Jiehe1998yd@gmail.com>
2018-04-25 12:34:54 +0100

    Added line chart on bar graph

--- app/src/main/java/com/example/jiehe/atracker/GraphFragment.java
+++ app/src/main/java/com/example/jiehe/atracker/GraphFragment.java
@@ -4,28 +4,39 @@ package com.example.jiehe.atracker;
  * Created by jiehe on 19/03/2018.
  */
 import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.CombinedData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
 import java.util.ArrayList;
 import java.util.Map;
 
 public class GraphFragment extends Fragment {
 
   //example chart object
-  BarChart myChart;
+  CombinedChart mChart;
   FileManager fm;
 
   Button btnPickDate1;
@@ -46,7 +57,37 @@ public class GraphFragment extends Fragment {
     View view = inflater.inflate(R.layout.frag_graph, container, false);
 
     //initialised myChart object
-    myChart = (BarChart) view.findViewById(R.id.BarChartExample);
+    mChart = (CombinedChart) view.findViewById(R.id.BarChartExample);
+    mChart.getDescription().setEnabled(false);
+    mChart.setBackgroundColor(Color.WHITE);
+    mChart.setDrawGridBackground(false);
+    mChart.setDrawBarShadow(false);
+    mChart.setHighlightFullBarEnabled(false);
+
+    // draw bars behind lines
+    mChart.setDrawOrder(new CombinedChart.DrawOrder[]{
+            CombinedChart.DrawOrder.BAR, CombinedChart.DrawOrder.BUBBLE, CombinedChart.DrawOrder.CANDLE, CombinedChart.DrawOrder.LINE, CombinedChart.DrawOrder.SCATTER
+    });
+
+    Legend l = mChart.getLegend();
+    l.setWordWrapEnabled(true);
+    l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+    l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+    l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+    l.setDrawInside(false);
+
+    YAxis rightAxis = mChart.getAxisRight();
+    rightAxis.setDrawGridLines(false);
+    rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+    YAxis leftAxis = mChart.getAxisLeft();
+    leftAxis.setDrawGridLines(false);
+    leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+
+    XAxis xAxis = mChart.getXAxis();
+    xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
+    xAxis.setAxisMinimum(0f);
+    xAxis.setGranularity(1f);
 
     btnPickDate1 = (Button)view.findViewById(R.id.btnPickDateOne);
     btnPickDate1.setOnClickListener(new View.OnClickListener() {
@@ -89,7 +130,7 @@ public class GraphFragment extends Fragment {
   /**
    * Plots a graph of start time (on x coord) against duration (on y coord)
    */
-  private void plotRecords(long lBound, long uBound) {
+  private BarData getBarData(long lBound, long uBound) {
     //load up the records from data base, the parameters are the start and end time of selection
     ArrayList<SingleActivityRecord> sSAR = new ArrayList<>();
     ArrayList<MyActivity> aActivity = fm.getActivity();
@@ -100,7 +141,7 @@ public class GraphFragment extends Fragment {
       sum = 0;
       sSAR = fm.getRecords(lBound, uBound, aActivity.get(i).getName());
       for(int j = 0; j < sSAR.size(); j++){
-         sum += sSAR.get(j).getDuration();
+         sum += (sSAR.get(j).getDuration() / 60000);
       }
       entries.add(new BarEntry(i, sum));
     }
@@ -113,16 +154,45 @@ public class GraphFragment extends Fragment {
       data.setBarWidth(0.9f);
 
       //set to display
-      myChart.setData(data);
-      myChart.setFitBars(true);
-      //refresh display
-      myChart.invalidate();
+      return data;
     }else{
       Snackbar s = Snackbar.make(this.getView(), "No records found!", Snackbar.LENGTH_LONG);
       s.show();
+      return null;
     }
   }
 
+  private LineData getLineData(){
+    ArrayList<MyActivity> aActivity = fm.getActivity();
+    LineData d = new LineData();
+    ArrayList<Entry> entries = new ArrayList<>();
+
+    if(aActivity.size() > 0){
+      for(int i = 0; i < aActivity.size(); i++){
+        entries.add( new Entry(i * 0.9f + 0.45f, aActivity.get(i).getActualGoal() * 60));
+        Log.d("GRAPH", (aActivity.get(i).getActualGoal() * 60) + " ");
+      }
+    }else{
+      return null;
+    }
+    Log.d("GRAPH", "HERE");
+    LineDataSet set = new LineDataSet(entries, "Line dataset");
+    set.setColor(Color.rgb(240, 238, 70));
+    set.setLineWidth(2.5f);
+    set.setCircleColor(Color.rgb(240, 238, 70));
+    set.setCircleRadius(5f);
+    set.setFillColor(Color.rgb(240, 238, 70));
+    set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+    set.setDrawValues(true);
+    set.setValueTextSize(10f);
+    set.setValueTextColor(Color.rgb(240, 238, 70));
+
+    set.setAxisDependency(YAxis.AxisDependency.LEFT);
+    d.addDataSet(set);
+
+    Log.d("GRAPH", "HERE");
+    return d;
+  }
   /**
    * Sets the FileManager
    * @param fm
@@ -134,19 +204,30 @@ public class GraphFragment extends Fragment {
 
   public void genGraph(){
     Snackbar mySnackbar = Snackbar.make(this.getView(), "Please fix the dates...", Snackbar.LENGTH_LONG);
+    CombinedData data = new CombinedData();
     if(Date1.dateInMillis > 0){
       if(Date2.dateInMillis == 0){
-        plotRecords(Date1.dateInMillis, Long.MAX_VALUE);
+        data.setData(getBarData(Date1.dateInMillis, Long.MAX_VALUE));
+        data.setData(getLineData());
+        mChart.setData(data);
+        mChart.invalidate();
+        mySnackbar.show();
       }else if(Date1.dateInMillis < Date2.dateInMillis){
-        plotRecords(Date1.dateInMillis, Date2.dateInMillis);
+        data.setData(getBarData(Date1.dateInMillis, Date2.dateInMillis));
+        data.setData(getLineData());
+
+        mChart.setData(data);
+        mChart.invalidate();
+        mySnackbar.show();
       }else{
         mySnackbar.show();
       }
     }else{
       mySnackbar.show();
-
     }
   }
+
+
 
 
 }
